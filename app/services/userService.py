05287a5cde772108fbd2fcf039dec1a043e484ad
kaiser24 <damsog38@gmail.com>
2022-11-05 13:07:37 -0500

    Fixed the update user to only update parameters provided and keep current parameters if not provided

--- app/services/userService.py
+++ app/services/userService.py
@@ -1,35 +1,55 @@
 from prisma import Prisma
 from ..models.userDTO import UserDTO, UserBaseDTO, UserUpdateDTO, UserCreateDTO
 from typing import List
+from ..libs.logger import Logger
+import asyncio
 
 class UserService:
     def __init__(self, database: Prisma):
         self.database = database
+        self.logger = Logger("INFO", "userService")
 
-    def get_all(self) -> List[UserDTO]:
-        users = self.database.user.find_many()
+    async def get_all(self) -> List[UserDTO]:
+        users = await self.database.user.find_many()
         return users
 
-    def get_by_id(self, id: str) -> UserDTO:
-        user = self.database.user.find_unique(
+    async def get_by_id(self, id: str) -> UserDTO:
+        user = await self.database.user.find_unique(
             where={"id": id}
         )
         return user
 
-    def create(self, user: UserCreateDTO) -> UserDTO:
-        user = self.database.user.create(
+    async def create(self, user: UserCreateDTO) -> UserDTO:
+        user = await self.database.user.create(
             data=user.dict()
         )
         return user
     
-    def update(self, id: str, user: UserUpdateDTO) -> UserDTO:
-        user = self.database.user.update(
-            data=user.dict(), 
+    # Updates user. updates fields that are not None. otherwises, 
+    # keeps the old value
+    async def update(self, id: str, user: UserUpdateDTO) -> UserDTO:
+        user_dict = user.dict()
+        
+        # Gets current user data
+        user_current = await self.database.user.find_unique(
+            where={"id": id}
+        ) 
+        if(not user_current): return None
+        user_current_dict = user_current.dict()
+
+        # If incomming data is null, keep current data
+        for key in user_dict:
+            if user_dict[key] is None:
+                user_dict[key] = user_current_dict[key]
+        
+        # Update user
+        user_updated = await self.database.user.update(
+            data=user_dict, 
             where={"id": id}
         )
-        return user
+        return user_updated
     
-    def delete(self, id: str) -> UserDTO:
-        user = self.database.user.delete(
+    async def delete(self, id: str) -> UserDTO:
+        user = await self.database.user.delete(
             where={"id": id}
         )
