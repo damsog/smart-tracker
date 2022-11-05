e623c7d03b8544c9fd493c0b495d0846ed3012c3
kaiser24 <damsog38@gmail.com>
2022-11-05 13:43:01 -0500

    being more specific to create the venv

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.7
-#python3 -m pip install virtualenv
-virtualenv smarttracker --python=python3.7
+python3.7 -m pip install virtualenv
+python3.7 -m virtualenv smarttracker
 source smarttracker/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install fastapi
