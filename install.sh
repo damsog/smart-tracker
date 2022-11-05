7f1350ff7ecd8db27a861388c92a271a96a43e36
kaiser24 <damsog38@gmail.com>
2022-11-05 13:06:39 -0500

    Added prisma python client and generation to install sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.7
-python3 -m pip install virtualenv
+#python3 -m pip install virtualenv
 virtualenv smarttracker --python=python3.7
 source smarttracker/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install fastapi
 python3 -m pip install "uvicorn[standard]"
+python3 -m pip install prisma
+prisma generate
+prisma migrate dev --name init
